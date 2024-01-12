1bdbb99c4f8df250b8a7c414ee7a6c834f821a0f
Filo2002 <talk.filippo@gmail.com>
2024-01-12 17:05:55 +0100

    Add terrain

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
+using Vector2 = UnityEngine.Vector2;
 
 public class Destroy : MonoBehaviour
 {
 
-    public Text text;
     public GameObject player;
     public GameObject platformPrefab;
     public GameObject springPrefab;
+    public GameObject terrainPrefab;
     public GameObject myPlat;
 
 
@@ -28,8 +33,15 @@ public class Destroy : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.name.StartsWith("Terrain"))
+        {
 
-        if(collision.gameObject.name.StartsWith("Platform"))
+                Destroy(collision.gameObject);
+         
+
+        }
+
+        if (collision.gameObject.name.StartsWith("Platform"))
         {
 
             if (Random.Range(1, 7) == 1)
@@ -37,16 +49,19 @@ public class Destroy : MonoBehaviour
 
                 Destroy(collision.gameObject);
                 Instantiate(springPrefab, new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f))), Quaternion.identity);
+                Instantiate(terrainPrefab, new Vector2(Random.Range(-7f, 7f), player.transform.position.y + (14 + Random.Range(-7f, 7f))), Quaternion.identity);
 
-
-            } else
+            }
+            else
             {
 
                 collision.gameObject.transform.position = new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f)));
 
             }
 
-        } else if(collision.gameObject.name.StartsWith("Spring"))
+        }
+
+        else if(collision.gameObject.name.StartsWith("Spring"))
         {
 
             if (Random.Range(1, 7) == 1)
@@ -67,14 +82,14 @@ public class Destroy : MonoBehaviour
         }
 
 
-       // Destroy(collision.gameObject);
+        // Destroy(collision.gameObject);
 
 
-       //  myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 0.8f))), Quaternion.identity);
-       //  Debug.Log("Created Normal");
+        //  myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 0.8f))), Quaternion.identity);
+        //  Debug.Log("Created Normal");
 
         //    if (Random.Range(1, 6) == 1)
-       //       {
+        //       {
 
         //       Instantiate(springPrefab, new Vector2(myPlat.transform.position.x, myPlat.transform.position.y + 0.4f), Quaternion.identity);
 
